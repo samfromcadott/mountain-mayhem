55ca547662cb40248cbe69cab15f2b1d718900a7
samfromcadott <samahstan@gmail.com>
2022-12-07 09:52:50 -0600

    Avoid seeing white space on upcoming segments

--- src/hill.cc
+++ src/hill.cc
@@ -27,7 +27,7 @@ void Hill::render() {
 
 		Vector2 road_points[] = { start, end, {end.x, end.y-road_width}, {start.x, start.y-road_width} };
 
-		DrawRectangle(start.x, start.y-256, segment_length, 512, grass_color);
+		DrawRectangle(start.x, start.y-512, segment_length, 1024, grass_color);
 		DrawTriangleFan(road_points, 4, road_color);
 
 		// Segment view for debugging
@@ -43,7 +43,7 @@ void Hill::render() {
 void Hill::update() {
 	// If the last segment is less than 128 pixels from the player generate more segments
 	float hill_length = segments.size() * segment_length;
-	if ( hill_length - 128 <= player.position.x ) {
+	if ( hill_length - 160 <= player.position.x ) {
 		// segments.push_back(Slope::FLAT);
 		// heights.push_back( heights.back() );
 		add_segments();
